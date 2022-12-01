13cbfa6cedc81a5129fa0ebae0f54828221e20fc
Toyosi Taiwo <adekunletaiwo43@yahoo.com>
2022-12-01 04:25:45 +0100

    Add a simple share post form

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,9 +1,96 @@
-import React from 'react'
+import React, { useState } from "react";
 
 export default function Form() {
+  const [postImage, setPostImage] = useState({
+    myFile: "",
+  });
+
+  const convertToBase64 = (file) => {
+    return new Promise((resolve, reject) => {
+      const fileReader = new FileReader();
+      fileReader.readAsDataURL(file);
+
+      fileReader.onload = () => {
+        resolve(fileReader.result);
+      };
+
+      fileReader.onerror = (error) => {
+        reject(error);
+      };
+    });
+  };
+
+  const handleFileUpload = async (e) => {
+    const file = e.target.files[0];
+    const base64 = await convertToBase64(file);
+    setPostImage({ ...postImage, myFile: base64 });
+  }
+  const handleSubmit = (e) => {};
   return (
-    <div>
-        <h1>Create a post</h1>
+    <div className="w-full max-w-xs">
+      <h1>Create a post</h1>
+      <form
+        onSubmit={handleSubmit}
+        class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+      >
+        <div class="mb-4">
+          <label class="block text-gray-700 text-sm font-bold mb-2" for="title">
+            Title
+          </label>
+          <input
+            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="title"
+            type="text"
+            placeholder="title"
+            required
+          />
+        </div>
+        <div class="mb-6">
+          <label class="block text-gray-700 text-sm font-bold mb-2" for="tags">
+            Tags
+          </label>
+          <input
+            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="tags"
+            type="text"
+            placeholder="Enter your tags"
+          />
+        </div>
+        <div class="mb-6">
+          <label
+            class="block text-gray-700 text-sm font-bold mb-2"
+            for="message"
+          >
+            Message
+          </label>
+          <textarea
+            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="message"
+            type="text"
+            placeholder="Enter your caption"
+            rows="6"
+            required
+          />
+        </div>
+        <div class="mb-6">
+        <input
+          type="file"
+          label="Image"
+          name="myFile"
+          accept=".jpeg, .png, .jpg"
+          multiple={false}
+          onChange={(e) => handleFileUpload(e)}
+        />
+        </div>
+        <div class="flex items-center justify-between">
+          <button
+            class="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            type="button"
+          >
+            Share
+          </button>
+        </div>
+      </form>
     </div>
-  )
+  );
 }
